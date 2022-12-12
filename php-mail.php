f213fcafd01a6b19b2954f9e850edb17cef89fa0
Matthew Setter <matthew@matthewsetter.com>
2022-12-12 11:44:21 +0100

    Update the email example which uses PHP's mail function
    This change updates the list of issues using mail includes,
    adds more headers to the email, and adds a nicer way of
    confirming to the user whether the email was sent or not.

--- php-mail.php
+++ php-mail.php
@@ -2,16 +2,25 @@
 
 /**
  * Send an email using PHP's built-in mail function
+ *
+ * There are a number of issues with using PHP's mail function, both directly
+ * and more globally. Mail servers may not accept the emails if they're from
+ * servers that are not properly configured, or if emails are not formatted
+ * correctly. You have to be familiar with a number of RFCs, so that the
+ * information entered is correctly formatted.
  */
 $status = mail(
-    "matthew@matthewsetter.com",
+    "Matthew Setter <matthew@matthewsetter.com>",
     "The subject of the email",
-    "Here is my message",
+    wordwrap("Here is my message", 70, "\r\n"),
     [
-        'Cc' => 'matthew.setter@gmail.com',
-        'Reply-To' => 'matthew.setter@gmail.com',
+        'Bcc' => 'Matthew Setter <matthew@protonmail.com>',
+        'Cc' => 'Matthew Setter <matthew.setter@gmail.com>',
+        'Reply-To' => 'Matthew Setter <matthew@matthewsetter.com>',
         'X-Mailer' => sprintf("PHP %s", phpversion()),
     ]
 );
 
-var_dump("Status is " . (bool)$status);
+echo (bool)$status
+    ? "Email was successfully sent"
+    : "Email was NOT successfully sent";
